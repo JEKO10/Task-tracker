6239bdd3edd39df49bafec3a4a4a921badb3a57b
Aleksa Bubanja <aleksa3b@gmail.com>
2021-11-03 20:47:05 +0100

    Tasks component setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
 import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
 // favicon
 // title
 function App() {
   const [isAddOpen, setIsAddOpen] = useState(false);
+  const [tasks, setTasks] = useState([
+    {
+      id: 0,
+      text: "Dentist Appointment",
+      day: "Jan 5th at 4:20PM",
+      reminder: true,
+    },
+    {
+      id: 1,
+      text: "Festival at school",
+      day: "Feb 9th at 2:40PM",
+      reminder: false,
+    },
+    {
+      id: 2,
+      text: "Shopping",
+      day: "Feb 15th at 3:00PM",
+      reminder: false,
+    },
+    {
+      id: 3,
+      text: "Final Exam",
+      day: "May 10th at 8:30AM",
+      reminder: true,
+    },
+  ]);
 
   return (
     <div className="main">
       <Header isAddOpen={isAddOpen} setIsAddOpen={setIsAddOpen} />
       {isAddOpen ? <AddTask /> : ""}
+      {tasks.length > 0 ? <Tasks tasks={tasks} /> : "There is no more tasks!"}
     </div>
   );
 }
