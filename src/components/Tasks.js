aa2408c4767c0cb7877359492dccdb1ff622826e
Aleksa Bubanja <aleksa3b@gmail.com>
2021-11-04 09:16:24 +0100

    Task component setup

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Task from "./Task";
 
-function Tasks() {
-  return <div></div>;
+function Tasks({ tasks }) {
+  return (
+    <div>
+      {tasks.map((task) => {
+        return <Task key={task.id} task={task} />;
+      })}
+    </div>
+  );
 }
 
 export default Tasks;
